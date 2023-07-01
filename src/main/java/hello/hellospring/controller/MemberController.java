36fa8a430007462a013cd256ccc2000a7c6babdc
Hwangseoeun <sunny030921@gmail.com>
2023-07-01 21:17:12 +0900

    MemberController

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -36,6 +36,7 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping(value = "/members")
     public String list(Model model){
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
